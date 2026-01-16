8ca35cff4c78183495f8b8a7a62d12f7784370a1
MVNFINHUB <253871551+MVNFINHUB@users.noreply.github.com>
2026-01-16 12:09:20 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,9 @@
-// js/config.js
-const config = {
-    // PASTE YOUR REAL KEYS HERE
-     SUPABASE_URL: "https://fviufivewglglnxhlmmf.supabase.co", 
+// MVN FINHUB SYSTEM CONFIGURATION
+const CONFIG = {
+   SUPABASE_URL: "https://fviufivewglglnxhlmmf.supabase.co", 
     SUPABASE_ANON_KEY: "sb_publishable_HYE7g0GyJbUfmldKTTAbeA_OUdc0Rah"
+    WHATSAPP_NUMBER: "919876543210", // No spaces, include country code
+    EMAIL_ID: "mvnfinhub.connect@gmail.com"
 };
-export default config;
+
+export default CONFIG;
